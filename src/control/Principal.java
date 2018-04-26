fe2c8de673d5aff4f6da687a699b2a4abe2294de
Gedalias dos Santos Caldas <gsantos.c18@gmail.com>
2018-04-26 12:58:17 -0300

    Adicionado o C3PO, porém não foi instalado. Realizado a limpesa do projeto

--- src/control/Principal.java
+++ src/control/Principal.java
@@ -5,13 +5,7 @@
  */
 package control;
 
-import java.net.URL;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.util.Scenario;
 
